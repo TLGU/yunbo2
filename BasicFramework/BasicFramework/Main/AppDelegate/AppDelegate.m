fed89c620b8145d5293bd76c0cb834f4fcec8f46
TLei <506466852@qq.com>
2016-10-21 17:49:32 +0800

    友盟推送配置
    友盟推送配置,暂时还未收到推送消息,检查p12证书的配置的正确性

--- BasicFramework/BasicFramework/Main/AppDelegate/AppDelegate.m
+++ BasicFramework/BasicFramework/Main/AppDelegate/AppDelegate.m
@@ -15,9 +15,13 @@
 #import <AVFoundation/AVFoundation.h>//测试代码
 #import <UMSocialCore/UMSocialCore.h>
 #import "WXApi.h"
-//#import "WXApiManager.h"
-@interface AppDelegate ()
+#import "UMessage.h"
+#import <UserNotifications/UserNotifications.h>
 
+
+//#import "WXApiManager.h"
+@interface AppDelegate ()<UNUserNotificationCenterDelegate>
+@property(strong,nonatomic)NSDictionary *userInfo;
 @end
 
 @implementation AppDelegate
@@ -77,6 +81,8 @@
     
     [self configUmeng];
     
+    [self configUmengPushWithLaunchOptions:launchOptions];
+    
     [[AppSingle Shared]  setReachability];
     
     [self setMyWindowAndRootViewController];
@@ -88,6 +94,70 @@
     
     return YES;
 }
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // 1.2.7版本开始不需要用户再手动注册devicetoken，SDK会自动注册
+//    [UMessage registerDeviceToken:deviceToken];
+    
+    
+}
+
+
+//iOS10以下使用这个方法接收通知
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    //开启友盟自带的弹出框
+    [UMessage setAutoAlert:YES];
+    [UMessage didReceiveRemoteNotification:userInfo];
+    
+        self.userInfo = userInfo;
+        //定制自定的的弹出框
+        if([UIApplication sharedApplication].applicationState == UIApplicationStateActive)
+        {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"标题"
+                                                                message:@"Test On ApplicationStateActive"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"确定"
+                                                      otherButtonTitles:nil];
+    
+            [alertView show];
+    
+        }
+}
+
+
+//iOS10新增：处理前台收到通知的代理方法
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler{
+    NSDictionary * userInfo = notification.request.content.userInfo;
+    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        //应用处于前台时的远程推送接受
+        //关闭友盟自带的弹出框
+        [UMessage setAutoAlert:NO];
+        //必须加这句代码
+        [UMessage didReceiveRemoteNotification:userInfo];
+        
+    }else{
+        //应用处于前台时的本地推送接受
+    }
+    //当应用处于前台时提示设置，需要哪个可以设置哪一个
+    completionHandler(UNNotificationPresentationOptionSound|UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionAlert);
+}
+
+//iOS10新增：处理后台点击通知的代理方法
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler{
+    NSDictionary * userInfo = response.notification.request.content.userInfo;
+    if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        //应用处于后台时的远程推送接受
+        //必须加这句代码
+        [UMessage didReceiveRemoteNotification:userInfo];
+        
+    }else{
+        //应用处于后台时的本地推送接受
+    }
+    
+}
+
 -(void)setMyWindowAndRootViewController
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
@@ -97,7 +167,35 @@
     BasicMainTBC *tbc=[BasicMainTBC new];
     self.window.rootViewController =tbc;
 }
+//友盟推送相关配置
+-(void)configUmengPushWithLaunchOptions:(NSDictionary*)launchOptions{
 
+    //初始化方法,也可以使用(void)startWithAppkey:(NSString *)appKey launchOptions:(NSDictionary * )launchOptions httpsenable:(BOOL)value;这个方法，方便设置https请求。
+    [UMessage startWithAppkey:@"580861b5e0f55a431a003b5a" launchOptions:launchOptions];
+    
+    
+    //注册通知，如果要使用category的自定义策略，可以参考demo中的代码。
+    [UMessage registerForRemoteNotifications];
+    
+    //iOS10必须加下面这段代码。
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate=self;
+    UNAuthorizationOptions types10=UNAuthorizationOptionBadge|  UNAuthorizationOptionAlert|UNAuthorizationOptionSound;
+    [center requestAuthorizationWithOptions:types10     completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            //点击允许
+            //这里可以添加一些自己的逻辑
+        } else {
+            //点击不允许
+            //这里可以添加一些自己的逻辑
+        }
+    }];
+    
+    //打开日志，方便调试
+    [UMessage setLogEnabled:YES];
+}
+
+//友盟登录分享相关配置
 -(void)configUmeng{
     //打开调试日志
     [[UMSocialManager defaultManager] openLog:YES];
@@ -105,6 +203,7 @@
     //设置友盟appkey
 //    [[UMSocialManager defaultManager] setUmSocialAppkey:@"57b432afe0f55a9832001a0a"];
     [[UMSocialManager defaultManager] setUmSocialAppkey:@"580861b5e0f55a431a003b5a"];
+    
     
     
     // 获取友盟social版本号
@@ -124,6 +223,10 @@
     
     // 如果不想显示平台下的某些类型，可用以下接口设置
     [[UMSocialManager defaultManager] removePlatformProviderWithPlatformTypes:@[@(UMSocialPlatformType_WechatFavorite)]];
+    
+    
+    
+    
     
     
 }
